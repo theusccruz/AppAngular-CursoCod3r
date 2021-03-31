7fc749e09f9db1fc335f2c75f0612663e5b683f4
theusccruz <matheus8476@outlook.com>
2021-03-30 21:42:05 -0300

    verificação de campos do form product-create

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -18,15 +18,20 @@ export class ProductCreateComponent implements OnInit {
   constructor(private _productService: ProductService,
     private _router: Router) { }
 
-  ngOnInit(): void {    
+  ngOnInit(): void {
   }
 
   createProduct(): void {
-    this._productService.create(this.product)
-    .subscribe(() => {
-      this._productService.showMessage(`Produto ${this.product.name} foi criado!`);
+    if (this.product.name === '' || this.product.price === null) {
+      this._productService.showMessage(`Nenhum produto foi criado`);
       this._router.navigate(['/products']);
-    });
+    } else {
+      this._productService.create(this.product)
+        .subscribe(() => {
+          this._productService.showMessage(`Produto ${this.product.name} foi criado!`);
+          this._router.navigate(['/products']);
+        });
+    }
   }
 
   cancel(): void {
